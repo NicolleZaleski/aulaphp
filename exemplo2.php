1eee60d1ab7add2d3e9fcbaa662541b7f659dd47
NicolleZaleski <nicolle.m.zaleski@gmail.com>
2024-10-31 20:46:43 -0400

    Aula php
    exemplos da aula php

---
+++ exemplo2.php
@@ -0,0 +1,7 @@
+<?php
+    $nome = "Nicolle";
+    $curso = "Análise e Desenvolvimento de Sistemas";
+    $profissao = "Estudante";
+
+    echo "Oi $nome. Parabéns pela sua faculdade de $curso.";
+?>
---
+++ retangulo.php
@@ -0,0 +1,7 @@
+<?php
+    $base = 3;
+    $altura = 8;
+    $area = $base * $altura;
+
+    echo $area;
+?>
